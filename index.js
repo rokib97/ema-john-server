c0252c2b566b6110e980d6ba1a7f39f287c7d768
rokib97 <hasanrokib1997@gmail.com>
2022-04-24 20:27:11 +0600

    added pagination api call

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awja5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJohn").collection("product");
+
+    // get all the product data
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const cursor = productCollection.find(query);
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+      res.send(products);
+    });
+
+    // count for pagination
+    app.get("/productCount", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    console.log("DB is connected");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// client.connect((err) => {
+//   const collection = client.db("emaJohn").collection("product");
+//   // perform actions on the collection object
+//   console.log("db is connected");
+//   client.close();
+// });
+
+app.get("/", (req, res) => {
+  res.send("John is running and waiting for ema");
+});
+
+app.listen(port, () => {
+  console.log("John is running on port", port);
+});
