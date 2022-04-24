0ef87cf09fa8fee66937662bf67d7ee9f16d83c0
rokib97 <hasanrokib1997@gmail.com>
2022-04-25 02:14:55 +0600

    search products by saved id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -44,6 +44,17 @@ async function run() {
     app.get("/productCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
+    });
+
+    // use post to get products by id's
+    app.post("/productByKeys", async (req, res) => {
+      const keys = req.body;
+      console.log(keys);
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     });
 
     console.log("DB is connected");
